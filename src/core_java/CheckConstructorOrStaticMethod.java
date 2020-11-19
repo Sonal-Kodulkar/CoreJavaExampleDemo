83861166503a9b9f152322291864dd83673a8fdc
Sonal <sonal.kodulkar@example.com>
2020-11-19 10:51:51 +0530

    initial commit

--- src/core_java/CheckConstructorOrStaticMethod.java
+++ src/core_java/CheckConstructorOrStaticMethod.java
@@ -1,17 +1,18 @@
 package core_java;
 
 public class CheckConstructorOrStaticMethod {
-    public CheckConstructorOrStaticMethod()
-    {
+    public CheckConstructorOrStaticMethod() {
         System.out.println("Constructor called");
     }
-    public static void display()
-    {
+
+    public static void display() {
         System.out.println("Static method called");
     }
 
     public static void main(String[] args) {
         CheckConstructorOrStaticMethod obj = new CheckConstructorOrStaticMethod();
         display();
+
+
     }
 }
--- src/core_java/stringExample/stringBufferExample.java
+++ src/core_java/stringExample/stringBufferExample.java
@@ -3,27 +3,27 @@ package core_java.stringExample;
 public class stringBufferExample {
     public static void main(String[] args) {
         StringBuffer sb = new StringBuffer("Perennial");
-        // Method 1: append() - concatenates the given argument with this practiceProblem.fibonacciesSeries.string
+        // Method 1: append() - concatenates the given argument with String.
         sb.append("System");
-        System.out.println("Initial Capacity of practiceProblem.fibonacciesSeries.string: " + sb.capacity());
+        System.out.println("Initial Capacity of string: " + sb.capacity());
         System.out.println("Output of append method: " + sb);
 
-        // Method 2 :insert()- inserts the given practiceProblem.fibonacciesSeries.string with this practiceProblem.fibonacciesSeries.string at the given position
+        // Method 2 :insert()- inserts the given string with this string at the given position
         sb.insert(0, "Pune");
-        System.out.println(sb);
+        System.out.println("Output after insert method : "+sb);
 
-        // Method 3: replace() - replaces the given practiceProblem.fibonacciesSeries.string from the specified beginIndex and endIndex.
+        // Method 3: replace() - replaces the given string from the specified beginIndex and endIndex.
         sb.replace(1, 3, "java");
-        System.out.println("Output of StringBuffer after replcae method: " + sb);
+        System.out.println("Output of StringBuffer after replace method: " + sb);
 
-        // Method 4: delete() - deletes the practiceProblem.fibonacciesSeries.string from the specified beginIndex to endIndex
+        // Method 4: delete() - deletes the string from the specified beginIndex to endIndex
         sb.delete(0, 6);
         System.out.println(sb);
 
         sb.append("Ichalkaranji");
         sb.append("Pune");
         System.out.println(sb);
-        System.out.println("Capacity of practiceProblem.fibonacciesSeries.string: " + sb.capacity());
+        System.out.println("Capacity of string: " + sb.capacity());
         sb.ensureCapacity(10);
         System.out.println(sb.capacity());
     }
